f02d0a701a395a317a16d9c7c8fd7bda1d05bb3d
RodwynLinio <rodwyn.moreno@linio.com>
2016-02-12 18:15:42 -0600

    this function add tasks to de database

--- app/scripts/controllers/tareas.js
+++ app/scripts/controllers/tareas.js
@@ -8,13 +8,19 @@
  * Controller of the hpAppApp
  */
 angular.module('hpAppApp')
-  .controller('TareasCtrl', function ($scope, $firebase) {
+  .controller('TareasCtrl', function ($scope, $firebaseArray) {
     //Configurar conexión a base de datos.
     var refTareas = new Firebase('https://listatareasrod.firebaseio.com/tareas');
 
-    $scope.tareas = $firebase(refTareas);
+    $scope.tareas = $firebaseArray(refTareas);
 
     $scope.restantes = function() {
-      return $scope.tareas.$getIndex().length;
+      //return $scope.tareas.$getIndex().length;
+      return 1;
     };
+    $scope.agregarTarea =  function() {
+      $scope.tareas.$add({texto: $scope.textoNuevaTarea, hecho:false});
+      $scope.textoNuevaTarea = '';
+    };
+
   });
